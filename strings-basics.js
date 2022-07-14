2bc95a7f3df6d230fe90663f866785c05c53a246
LoganM3 <mccalleylogan@gmail.com>
2022-07-14 11:47:05 -0400

    practice of strings

---
+++ strings-basics.js
@@ -0,0 +1,23 @@
+// const message = '     Hello Javascript Students    ';
+// const msgToLowerCase = message.toLowerCase();
+// const msgToUpperCase = message.toUpperCase();
+// const messageNoSpace = message.trim();
+// const newMessage = message.replace('Students', 'Engineers');
+// const withSubString = message.substring(5, 14);
+// message.startsWith('Hello');
+
+// console.log(message);
+// console.log(msgToLowerCase);
+// console.log(msgToUpperCase);
+// console.log(messageNoSpace);
+// console.log(newMessage);
+// console.log(withSubString);
+// console.log(messageNoSpace.startsWith('Hello'));
+
+const mySchool = 'Boca Code'
+const myIndex = mySchool.indexOf('Code')
+console.log(myIndex)
+
+const myString = 'Hello-level-up-students!'
+const newString = myString.split(' ')
+console.log(newString)
